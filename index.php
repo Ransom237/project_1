50a659918d1454c47036c7293eeb1422b73af3fb
unknown <chenwiransom9@gmail.com>
2022-07-27 12:46:06 +0100

    modify file

---
+++ index.php
@@ -0,0 +1,75 @@
+
+<!Doctype html>
+<html> 
+  <head>
+    <title>My Article</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <style>
+    div {
+      width: 500px;
+      padding: 3px;
+      border: 5px solid darkviolet;
+      margin: 20px;
+      background-color: rgba(123, 255, 0, 0.7);
+    }
+    .center {
+      width: 500px;
+      padding: 10px;
+      border: 5px solid yellow ;
+      margin: 20px;
+      background-color: rgb(0,255,0,0.7);
+    }
+
+    body {
+      background-image:
+    url("images/MA11.jpg");
+    }
+    h1 {
+       color:red;
+       text-align:center;
+    }
+    h2 {
+       text-align:center;
+    }
+  </style>
+  </head>
+  <body>
+    <article>
+    <section>
+      <article>
+        <h1><ins><b>HEALTH AND HYGIENE</b></ins></h1>
+        <h4><ins>Definition</ins></h4>
+        <p>Health and Hygiene refer to the overall well-being-physical,social,mental practice to ensure that we have good health</p><br>
+      <div>
+         <div class="center">> 
+          <h3><b><ins>ADVANTAGES OF PROPER HYGIENE.</ins></b></h3>
+              <ul><li>Protects our body from diseases and helps our imune system become more robust.</li></ul> 
+              <ul><li>Free from different microorganism</li></ul>
+              <ul><li>Help the body produce antibody nicely without much effort</li></ul>
+           <h3><b><ins> DISADVANTAGE OF POOR HYGIENE.</ins></b></h3>   
+              <ul><li>
+                  Lack of regular brushing and flossing leaves small food particle wedged between the teeth that collects bacteria and emit chemicals,like hydrogen sulfide-<strong> the same compunds that give <mark>rotten eggs</mark> thier smell.</strong>
+              </li></ul> 
+              <ul><li>Leads to bad breath and other oral health concern</li></ul>
+              <ul><li>
+                  When a person doesn't practice good hhygiene ,thier body can accumulate bacteria that contribute to diseases such as athlet foot, headlice and scabies
+              </li></ul>
+              <ul><li>Poor hygien can alos cause parasitesto grow an grow on the skin</li></ul>
+         </div>
+      </div><br>
+	<h1><b><ins> WAYS TO MIANTAIN GOOD HYGIENE</ins></b></h1>
+        <p>The Miantenance of personal hygiene is health is necessary as it helps to protect the health and total wellness of individuals. Not only is this directly 		advantageous to the personal health of a specific person. Poor individual health can not only lead to physicl concern but can also impact an individual 		emotionally and socially.see more.
+	</p>
+	<p> Pure drinking water is vital cause it keeps us hydrated and helps in functioning of the organs and removind toxins. Untreated water cuases diseases like 		typhoid and cholera.
+	</p>
+	<p>Cleanliness is also imperative. Taking a bath regularly and waering clean clothes improve our hygiene. Washing hands frequently, cutting nails, and brushing 	as well as flossing daily also help enhance personal hygiene.
+	</p>
+        <p>Sounds good rigth</p>
+      <div>
+	  <p><i><marquee>BY CHENWI RANSOM</marquee></i></p>
+      </div><br>
+      </article>
+    </section>
+    <a href="My file.html">  <img src="images/MA1.png" alt="an image" > </a>
+  </body>
+</html>
